8d8ba8c2487ec9b9cbdd43e2c9559b81ee52f762
laiz-in <laizin2107@gmail.com>
2023-03-21 17:43:57 +0400

    sdfsd

--- app.py
+++ app.py
@@ -20,25 +20,26 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+        # data=CustomData(
+        #     gender=request.form.get('gender'),
+        #     race_ethnicity=request.form.get('ethnicity'),
+        #     parental_level_of_education=request.form.get('parental_level_of_education'),
+        #     lunch=request.form.get('lunch'),
+        #     test_preparation_course=request.form.get('test_preparation_course'),
+        #     reading_score=float(request.form.get('reading_score')),
+        #     writing_score=float(request.form.get('writing_score'))
 
-        )
-        pred_df=data.get_data_as_data_frame()
+        # )
+        # pred_df=data.get_data_as_data_frame()
 
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        results=int(math.floor(min(results[0],100)))
+        # predict_pipeline=PredictPipeline()
+        # results=predict_pipeline.predict(pred_df)
+        # results=int(math.floor(min(results[0],100)))
+        results=74
         output= f"the predicted math score is {results}"
         logging.info("predicted the output")
         return render_template('home.html',results=output)
     
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(port="5000",debug=False)
