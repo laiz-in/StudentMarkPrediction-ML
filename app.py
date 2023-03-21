9133f921a03d01a490e4d52ddba8fc78b26c31bd
laiz-in <laizin2107@gmail.com>
2023-03-21 13:48:22 +0400

    chnages form app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from src.logger import logging
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
-app=application
+app=Flask(__name__)
+
 
 @app.route('/')
 def index():
